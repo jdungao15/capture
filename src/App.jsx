1c3d38331a38febef11c819520343ffb1a11fc70
John Dungao <johnmiguel.dungao@gmail.com>
2023-05-25 02:33:06 -0400

    add AboutUs.jsx and AboutSection.jsx components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import AboutUs from "./pages/AboutUs.jsx";
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+     <div className="App">
+       <AboutUs/>
+     </div>
   )
 }
 
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutSection from "../components/AboutSection.jsx";
+//Page Components
+const AboutUs = () => {
+   return (
+     <AboutSection/>
+   );
+};
+
+export default AboutUs;
