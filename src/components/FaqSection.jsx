dc44ccd5ab27453387540cba206a8770daefd263
John Dungao <johnmiguel.dungao@gmail.com>
2023-05-25 05:29:17 -0400

    added FaqSection.jsx

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const FaqSection = () => {
+   return (
+      <div className="faq">
+         <h2>Any Question <span>FAQ</span></h2>
+         <div className="question">
+            <h4>How Do I Start?</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+         <div className="question">
+            <h4>Daily Schedule</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+         <div className="question">
+            <h4>Different Payments</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+         <div className="question">
+            <h4>What Products do you offer?</h4>
+            <div className="answer">
+               <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from "../components/AboutSection.jsx";
 import ServicesSection from "../components/ServicesSection.jsx";
+import FaqSection from "../components/FaqSection.jsx";
 //Page Components
 const AboutUs = () => {
    return (
       <>
          <AboutSection/>
          <ServicesSection/>
+         <FaqSection/>
       </>
    );
 };
