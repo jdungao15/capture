b737c1cca16a62db74554ed3caf0c22eaafd6183
John Dungao <johnmiguel.dungao@gmail.com>
2023-05-25 05:58:38 -0400

    styles AboutSection.jsx using styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import AboutUs from "./pages/AboutUs.jsx";
+import GlobalStyle from "./components/GlobalStyle.jsx";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
      <div className="App">
+        <GlobalStyle/>
        <AboutUs/>
      </div>
   )
