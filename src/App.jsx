48371982c305a1339d86635c771b11d591053ac3
John Dungao <johnmiguel.dungao@gmail.com>
2023-05-26 03:35:02 -0400

    added routes on navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import {useState} from 'react';
 import AboutUs from "./pages/AboutUs.jsx";
 import GlobalStyle from "./components/GlobalStyle.jsx";
 import Nav from "./components/Nav.jsx";
-
+import ContactUs from "./pages/ContactUs.jsx";
+import OurWork from "./pages/OurWork.jsx";
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0);
 
-  return (
-     <div className="App">
-        <GlobalStyle/>
-        <Nav/>
-       <AboutUs/>
-     </div>
-  )
+   return (
+      <div className="App">
+         <GlobalStyle/>
+         <Nav/>
+         <Routes>
+            <Route path="/" element={<AboutUs/>}/>
+            <Route path="/contact" element={<ContactUs/>}/>
+            <Route path="/work" element={<OurWork/>}/>
+         </Routes>
+      </div>
+   );
 }
 
-export default App
+export default App;
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const ContactUs = () => {
+   return (
+      <div>
+         <h1>Contact Us </h1>
+      </div>
+   );
+};
+
+export default ContactUs;
