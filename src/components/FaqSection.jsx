62981292f9f2ee5b3d20d85bf4ea07efc9aa7a24
John Dungao <johnmiguel.dungao@gmail.com>
2023-05-31 05:52:34 -0400

    added toggle and animation on faq section.

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,67 +1,84 @@
 import React from 'react';
-import styled from 'styled-components'
-import {About} from '../styles.js'
+import styled from 'styled-components';
+import {About} from '../styles.js';
+import {Toggle} from "./Toggle.jsx";
+import {LayoutGroup} from "framer-motion";
 
 const FaqSection = () => {
    return (
       <Faq>
          <h2>Any Question <span>FAQ</span></h2>
-         <div className="question">
-            <h4>How Do I Start?</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, asperiores commodi cumque distinctio eius error illum nam nostrum, perspiciatis quas quos recusandae unde? Itaque labore laudantium, nesciunt quos repellat veniam?</p>
-            </div>
-            <div className="faq-line"></div>
-         </div>
-         <div className="question">
-            <h4>Daily Schedule</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid assumenda consequuntur cumque expedita, illo molestias officiis perspiciatis placeat quam quasi similique soluta veritatis! Animi, aspernatur odio? Aliquid, eligendi, quibusdam.</p>
-            </div>
-            <div className="faq-line"></div>
-         </div>
-         <div className="question">
-            <h4>Different Payments</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur earum error, et excepturi fuga id iusto mollitia nemo neque obcaecati officiis placeat quo repellat saepe sed unde veniam, vero?</p>
-            </div>
-            <div className="faq-line"></div>
-         </div>
-         <div className="question">
-            <h4>What Products do you offer?</h4>
-            <div className="answer">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias, animi architecto aspernatur, deleniti dolorum error in inventore iste iure, magni nesciunt nostrum odit possimus quaerat rerum soluta ut veritatis?</p>
-            </div>
-         </div>
-         <div className="faq-line"></div>
+         <LayoutGroup>
+            <Toggle layoutId='faq1' title="How do I Start?">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                     Aliquam, asperiores commodi cumque distinctio eius error
+                     illum nam nostrum, perspiciatis quas quos recusandae unde?
+                     Itaque labore laudantium, nesciunt quos repellat
+                     veniam?</p>
+               </div>
+
+            </Toggle>
+            <Toggle layoutId='faq2' title="Daily Schedule">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                     Alias aliquid assumenda consequuntur cumque expedita, illo
+                     molestias officiis perspiciatis placeat quam quasi similique
+                     soluta veritatis! Animi, aspernatur odio? Aliquid, eligendi,
+                     quibusdam.</p>
+               </div>
+            </Toggle>
+            <Toggle layoutId='faq3' title="Different Payments">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                     Aliquam consectetur earum error, et excepturi fuga id iusto
+                     mollitia nemo neque obcaecati officiis placeat quo repellat
+                     saepe sed unde veniam, vero?</p>
+               </div>
+            </Toggle>
+            <Toggle layoutId='faq4' title="What Products do you offer?">
+               <div className="answer">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                     alias, animi architecto aspernatur, deleniti dolorum error in
+                     inventore iste iure, magni nesciunt nostrum odit possimus
+                     quaerat rerum soluta ut veritatis?</p>
+               </div>
+            </Toggle>
+         </LayoutGroup>
       </Faq>
    );
 };
 const Faq = styled(About)`
   display: block;
-  span {
+
+  span { 
     display: block;
   }
+
   h2 {
     padding-bottom: 2rem;
     font-weight: lighter;
   }
+
   .faq-line {
     background: #cccccc;
     height: 0.2rem;
     margin: 2rem 0rem;
     width: 100%;
   }
+
   .question {
     padding: 2rem 0rem;
     cursor: pointer;
   }
+
   .answer {
     padding: .3rem 0rem;
+
     p {
       padding: 1rem 0rem;
     }
   }
-`
+`;
 
 export default FaqSection;
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {motion} from "framer-motion";
+import {titleAnim} from "../animation.js";
+
+export const Toggle = ({children, title, layoutId}) => {
+   const [toggle, setToggle] = useState(true);
+   // const paraAnim = {
+   //    hidden: {
+   //       opacity: 0,
+   //    },
+   //    visibility: {
+   //       opacity: 1,
+   //       transition: {
+   //          duration: 1
+   //
+   //       }
+   //    }
+   // }
+   const childrenContext = (
+      <motion.div
+         initial={{ opacity: 0, scale: 0.5 }}
+         animate={{ opacity: 1, scale: 1 }}
+         transition={{ duration: 0.5 }}
+      >{children}</motion.div>
+   )
+   return (
+      <motion.div layout layoutId={layoutId} className="question" onClick={()=> setToggle(!toggle)}>
+         <motion.h4 layout>{title}</motion.h4>
+         {toggle ? childrenContext : ""}
+         <div className="faq-line"></div>
+      </motion.div>
+   );
+};
